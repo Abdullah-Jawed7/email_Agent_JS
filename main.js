96efc91f214c54a467b6ece4a535a2e91ef10da9
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2025-08-13 13:44:13 +0500

    updates

--- main.js
+++ main.js
@@ -4,27 +4,8 @@ import {Triage_Agent} from "./customAgents.js"
 dotenv.config()
 
 async function main() {
-   const prompt = "I am Abdullah, want to send email to my friend Khalid at: khalidjawed23@gmail.com , Telling him about my new AI Agent that can write email according to instructions and send it to respective email that mentioned in instructions, telling benefits and uses of this AI agent , Asking him to share ideas to create more creative AI agents and use them more productive"
+   const prompt = "I am Abdullah, want to send email to my friend Khalid at: khalid@gmail.com , Telling him about my new AI Agent that can write email according to instructions and send it to respective email that mentioned in instructions, telling benefits and uses of this AI agent , Asking him to share ideas to create more creative AI agents and use them more productive"
    const result = await runner.run(Triage_Agent,  prompt);
    console.log(result.finalOutput)
 }
 main()
-
-
-
-
-
-
-
-
-
-
-
-                              `I am Abdullah, want to send email to my friend
-                               Khalid at: khalid@example.com , Telling him about
-                               my new AI Agent that can write email according
-                               to instructions and send it to respective email
-                               that mentioned in instructions, telling benefits
-                               and uses of this AI agent , Asking him to share 
-                               ideas to create more creative AI agents and use 
-                               them more productive`
